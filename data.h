0706e91be46ac2fbc8f5a7d9f7631a410e441abc
Aiden Dao <153690256+aiddao@users.noreply.github.com>
2026-03-12 17:58:51 +0000

    Created Restaurant and Food structs

--- data.h
+++ data.h
@@ -1,6 +1,20 @@
 #ifndef DATA_H
 #define DATA_H
 
+typedef struct Restaurant {
+    char *name;
+    char *cuisine;
+    char *type;
+    char *price_range;
+    Food *menu;
+    int menu_size;
+} Restaurant;
 
+typedef struct Food {
+    char *name;
+    char *additions;
+    char *subtractions;
+    double price;
+} Food;
 
 #endif // DATA_H
